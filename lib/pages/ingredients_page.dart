e0b54c33899da15f8c2186a7ab32264f96a1a63a
dfaAtGit <dfa.coder@gmail.com>
2023-05-26 21:17:08 +0200

    add ingredient page, add ingredient object and hive db

--- lib/pages/ingredients_page.dart
+++ lib/pages/ingredients_page.dart
@@ -6,7 +6,6 @@ import '../utils/ingredient_box.dart';
 
 class IngredientsPage extends StatefulWidget {
   final String name;
-  final _ingredients = Hive.box("ingredients");
 
   IngredientsPage({super.key, required this.name});
 
@@ -15,8 +14,19 @@ class IngredientsPage extends StatefulWidget {
 }
 
 class _IngredientsPageState extends State<IngredientsPage> {
+  final _ingredients = Hive.box("ingredients");
+  final units = [Unit.mg, Unit.g, Unit.kg, Unit.ml, Unit.l, Unit.stk];
+  var defaultUnit = Unit.g;
+  TextEditingController _nameController = new TextEditingController();
+  TextEditingController _amountController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    var ingredient = Ingredient()
+      ..name = "Hühnchen"
+      ..amount = "2"
+      ..unit = Unit.stk;
+
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 41, 37, 37),
         body: Center(
@@ -27,13 +37,105 @@ class _IngredientsPageState extends State<IngredientsPage> {
                     border: Border.all(width: 2.0, color: Colors.amber),
                     color: const Color.fromARGB(255, 41, 37, 37)),
                 height: 450,
-                width: 380,
-                child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  itemCount: 0,
-                  itemBuilder: (context, index) {
-                    return IngredientBox(ingredient: Ingredient());
-                  },
+                width: 400,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 350,
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return IngredientBox(ingredient: ingredient);
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(40),
+                          child: Container(
+                            height: 70,
+                            color: Colors.black26,
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10),
+                                Container(
+                                    width: 150,
+                                    height: 80,
+                                    child: const Center(
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextField(
+                                          style: TextStyle(color: Colors.amber),
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            labelStyle:
+                                                TextStyle(color: Colors.white),
+                                            labelText: 'Zutat..',
+                                            enabledBorder: UnderlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.blue),
+                                            ),
+                                            focusedBorder: UnderlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.green),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )),
+                                Container(
+                                    width: 100,
+                                    height: 80,
+                                    child: const Center(
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextField(
+                                          style: TextStyle(color: Colors.amber),
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            labelStyle:
+                                                TextStyle(color: Colors.white),
+                                            labelText: 'Menge..',
+                                            enabledBorder: UnderlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.blue),
+                                            ),
+                                            focusedBorder: UnderlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.green),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )),
+                                Container(
+                                  width: 60,
+                                  child: DropdownButton<Unit>(
+                                    value: defaultUnit,
+                                    dropdownColor: Colors.black,
+                                    items: units
+                                        .map((item) => DropdownMenuItem<Unit>(
+                                              value: item,
+                                              child: Text(
+                                                unitLabels[item]!,
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Colors.white),
+                                              ),
+                                            ))
+                                        .toList(),
+                                    onChanged: (item) =>
+                                        setState(() => defaultUnit = item!),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                    )
+                  ],
                 )),
           ),
         ));
